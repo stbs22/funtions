d03fc019c66bb4525704a2ea5974a4cc7a0d1b67
stbs22 <steveehs2000@gmail.com>
2021-05-10 17:14:30 -0400

    C/Ficheros.c
    Data/FCD/Data_limpio.csv
    Data/FCD/data.csv
    Data/FCD/data_continenete.csv
    Data/FCD/datacountry.csv
    Data/FCD/datap1.csv
    Data/FCD/limpia.csv
    Data/FCD/regions.csv
    Java/ctm.java
    Java/first.java
    Python/Graficos.ipynb
    Python/analisis_con_listas.py
    R/commands.r

---
+++ Java/first.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class first {
+
+  string ctm;
+  Animal(String nombre){
+    ctm = nombre;
+  }
+  public static void main(String[] args){
+    Scanner fuck = new Scanner(System.in);
+    System.out.print("pon la wea ctm: ");
+    
+    int a = fuck.nextInt();  
+    System.out.println(a);  
+  }
+}

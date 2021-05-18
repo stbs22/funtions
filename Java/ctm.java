4daa99296b6160843a5dde1b22233dcbd8aadc46
stbs22 <steveehs2000@gmail.com>
2021-05-18 01:47:59 -0400

    Java/ctm.java Java/first.java Java/util.java

--- Java/ctm.java
+++ Java/ctm.java
@@ -1,3 +1,27 @@
 public class ctm {
+
+    String atributo1;
+    int atributo2;
+
+    ctm(String atributo1, int atributo2){
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+    }
     
+    public static String getAtributo1(){
+        return atributo1;
+    }
+
+    public static int getAtributo2(){
+        return atributo2;
+    }
+
+    public static void setAtributo1(String nombre){
+        this.atributo1 = nombre;
+    }
+
+    public static void setAtributo1(String numero){
+        this.atributo2 = numero;
+    }
+
 }
--- Java/first.java
+++ Java/first.java
@@ -1,15 +1,16 @@
 import java.util.*;
-class first {
 
-  string ctm;
-  Animal(String nombre){
-    ctm = nombre;
-  }
+class first {
   public static void main(String[] args){
+
+    ctm weadeobjeto = new ctm("texto",1);
+
     Scanner fuck = new Scanner(System.in);
-    System.out.print("pon la wea ctm: ");
+
+    System.out.print("pon la wea  de numero ctm: ");
     
     int a = fuck.nextInt();  
-    System.out.println(a);  
+
+    System.out.println("["+a+"] webeo objeto string: "+weadeobjeto.atributo1);  
   }
 }
